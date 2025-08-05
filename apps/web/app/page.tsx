f0b1c59fb46696c76c1f2abf032ae40e47eb0747
Krish Koria <krishkoria2004@gmail.com>
2025-08-05 20:58:15 +0530

    Integrate Clerk authentication and layout components; add sign-in and sign-up pages

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,23 @@
 "use client";
+import { SignInButton, UserButton } from "@clerk/nextjs";
 import { api } from "@workspace/backend/_generated/api";
-import { useQuery } from "convex/react";
+import { Authenticated, Unauthenticated, useQuery } from "convex/react";
 
 export default function Page() {
   const users = useQuery(api.users.getUsers);
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <h1 className="text-2xl font-bold">Hello World from web</h1>
-      <pre>{JSON.stringify(users, null, 2)}</pre>
-    </div>
+    <>
+      <Authenticated>
+        <div className="flex flex-col items-center justify-center min-h-svh">
+          <UserButton />
+          <h1 className="text-2xl font-bold">Hello World from web</h1>
+          <pre>{JSON.stringify(users, null, 2)}</pre>
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        <p>Must be Authenticated</p>
+        <SignInButton>Sign In</SignInButton>
+      </Unauthenticated>
+    </>
   );
 }
