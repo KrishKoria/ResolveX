d42e15d90a509d35dd3c51f1b73c3793c99e1d37
Krish Koria <krishkoria2004@gmail.com>
2025-08-16 10:36:09 +0530

    Implement organization selection page and update middleware for organization redirection

--- apps/web/app/(auth)/org-select/[[...org-select]]/page.tsx
+++ apps/web/app/(auth)/org-select/[[...org-select]]/page.tsx
@@ -1,1 +1,5 @@
- 
+import OrganizationSelect from "@/components/auth/ui/org-select";
+
+export default function OrganizationSelectPage() {
+  return <OrganizationSelect />;
+}
--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,9 +1,24 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isOrgFreeRoute = createRouteMatcher([
+  "/org-select(.*)",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+]);
 export default clerkMiddleware(async (auth, req) => {
+  const { userId, orgId } = await auth();
   if (!isPublicRoute(req)) {
     await auth.protect();
+  }
+  if (userId && !isOrgFreeRoute(req) && !orgId) {
+    const searchParams = new URLSearchParams({ redirectTo: req.url });
+    const orgSelect = new URL(
+      `/org-select?${searchParams.toString()}`,
+      req.url
+    );
+    return NextResponse.redirect(orgSelect);
   }
 });
 
